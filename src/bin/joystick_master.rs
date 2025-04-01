5f9b8e3133e1f2daf5ef93cf3b0509d4384d5506
zayats1 <zaiats.bogdan@gmail.com>
2025-04-01 22:31:50 +0300

    merge unstable changes into main + small QOL improvements

--- src/bin/joystick_master.rs
+++ src/bin/joystick_master.rs
@@ -23,11 +23,17 @@ use esp_hal::{
 use esp_println::println;
 use esp_wifi::{EspWifiController, esp_now::PeerInfo, init};
 use heapless::String;
-use robo_remote::{self as _, mk_static};
+use robo_remote::{self as _, Map, mk_static};
 
 const ADC_SHIFT: u16 = 2144; // to obtain zero at the minimum of a joystick range
 
 const PEER_ADDRESS: [u8; 6] = [0x54, 0x32, 0x04, 0x32, 0xf2, 0xb8];
+
+#[cfg(debug_assertions)]
+const INTERVAL: Duration = Duration::from_millis(500);
+
+#[cfg(not(debug_assertions))]
+const INTERVAL: Duration = Duration::from_nanos(10);
 
 // TODO: master address
 #[esp_hal_embassy::main]
@@ -58,7 +64,6 @@ async fn main(_spawner: Spawner) -> ! {
     let systimer = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(systimer.alarm0);
 
-    let mut ticker = Ticker::every(Duration::from_millis(500)); // todo: make faster
     let mut data: String<64> = String::new();
 
     let analog_pin = peripherals.GPIO1;
@@ -75,9 +80,6 @@ async fn main(_spawner: Spawner) -> ! {
     let mut adc12 = Adc::new(adc.borrow_mut(), adc12_config);
 
     loop {
-        let x = adc1.read_oneshot(&mut pin).await.saturating_sub(ADC_SHIFT);
-        println!("X value: {}", x);
-
         if !esp_now.peer_exists(&PEER_ADDRESS) {
             esp_now
                 .add_peer(PeerInfo {
@@ -89,18 +91,30 @@ async fn main(_spawner: Spawner) -> ! {
                 .unwrap();
         }
 
+        Timer::after(Duration::from_millis(250)).await;
+
+        data.clear();
+
+        let x = adc1.read_oneshot(&mut pin).await.saturating_sub(ADC_SHIFT);
+        println!("X value: {}", x);
+
         let y = adc12
             .read_oneshot(&mut pin2)
             .unwrap_or_default()
             .saturating_sub(ADC_SHIFT);
         println!("Y value: {}", y);
-        Timer::after(Duration::from_millis(250)).await;
 
-        data.clear();
+        let x = ((x / 10) as f32)
+            .map(0.0, 176.0, -255.0, 255.0)
+            .clamp(-255.0, 255.0);
+        let y = ((y / 10) as f32)
+            .map(0.0, 176.0, -255.0, 255.0)
+            .clamp(-255.0, 255.0);
+        println!("X normed: {}", x);
+        println!("Y normed: {}", y);
         writeln!(&mut data, "X:{};Y:{};", x, y).unwrap(); // todo
         let status = esp_now.send_async(&PEER_ADDRESS, data.as_bytes()).await;
         println!("Send broadcast status: {:?}", status);
-
-        ticker.next().await;
+        Timer::after(INTERVAL).await;
     }
 }
