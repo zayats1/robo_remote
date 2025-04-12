05c0caf1e069664916fc820c9d8ab37beccea6f2
zayats1 <zaiats.bogdan@gmail.com>
2025-04-12 18:44:25 +0300

    rc_car begin

---
+++ src/bin/rc_car.rs
@@ -0,0 +1,102 @@
+//! ESP-NOW Example
+//!
+//! Broadcasts, receives and sends messages via esp-now
+
+//% FEATURES: esp-wifi esp-wifi/esp-now esp-hal/unstable
+//% CHIPS: esp32 esp32s2 esp32s3 esp32c2 esp32c3 esp32c6
+
+#![no_std]
+#![no_main]
+
+use core::str::{self};
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_alloc as _;
+use esp_backtrace as _;
+use esp_hal::{
+    clock::CpuClock, mcpwm::{operator::PwmPinConfig, timer::PwmWorkingMode, McPwm, PeripheralClockConfig}, peripherals::GPIO, rng::Rng, time::Rate, timer::timg::TimerGroup, uart::{self, Uart}
+};
+use esp_println::println;
+use esp_wifi::{EspWifiController, esp_now::PeerInfo, init};
+use robo_remote::{self as _, mk_static};
+
+const THE_ADDRESS: [u8; 6] = [0x54u8, 0x32, 0x04, 0x32, 0xf2, 0xb8];
+const WIFI_CHANNEL: u8 = 3;
+
+// so MCU shouldn't halt
+const INTERVAL: Duration = Duration::from_nanos(1);
+
+// TODO: master address
+#[esp_hal_embassy::main]
+async fn main(_spawner: Spawner) -> ! {
+    esp_println::logger::init_logger_from_env();
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+
+    let esp_wifi_ctrl = &*mk_static!(
+        EspWifiController<'static>,
+        init(
+            timg0.timer0,
+            Rng::new(peripherals.RNG),
+            peripherals.RADIO_CLK,
+        )
+        .unwrap()
+    );
+
+    let wifi = peripherals.WIFI;
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(esp_wifi_ctrl, wifi).unwrap();
+    println!("esp-now version {}", esp_now.version().unwrap());
+    esp_now.set_channel(WIFI_CHANNEL).unwrap();
+    use esp_hal::timer::systimer::SystemTimer;
+    let systimer = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(systimer.alarm0);
+
+
+
+// initialize peripheral
+let clock_cfg = PeripheralClockConfig::with_frequency(Rate::from_mhz(32)).unwrap();
+let mut mcpwm = McPwm::new(peripherals.MCPWM0, clock_cfg);
+
+// connect operator0 to timer0
+mcpwm.operator0.set_timer(&mcpwm.timer0);
+// connect operator0 to pin
+let mut pwm_pin = mcpwm
+    .operator0
+    .with_pin_a(peripherals.GPIO18, PwmPinConfig::UP_ACTIVE_HIGH);
+
+// start timer with timestamp values in the range of 0..=99 and a frequency
+// of 20 kHz
+let timer_clock_cfg = clock_cfg
+    .timer_clock_with_frequency(99, PwmWorkingMode::Increase,
+Rate::from_khz(20)).unwrap(); mcpwm.timer0.start(timer_clock_cfg);
+
+// pin will be high 50% of the time
+pwm_pin.set_timestamp(100);
+
+
+    loop {
+        let r = esp_now.receive_async().await;
+        if r.info.dst_address == THE_ADDRESS {
+            let data = r.data();
+            let rec = str::from_utf8(data).unwrap_or("Data is not received properly");
+    
+            println!("Received {}", rec);
+            if !esp_now.peer_exists(&r.info.src_address) {
+                esp_now
+                    .add_peer(PeerInfo {
+                        peer_address: r.info.src_address,
+                        lmk: None,
+                        channel: None,
+                        encrypt: false,
+                    })
+                    .unwrap();
+            }
+        }
+        Timer::after(INTERVAL).await;
+    }
+}
